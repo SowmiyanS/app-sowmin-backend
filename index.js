ab4ddc780faa09d383b194735159307a6c6e3122
SowmiyanS <sowmiyan9491@gmail.com>
2023-02-12 16:36:51 +0530

    added toArray() function to parse the data

--- index.js
+++ index.js
@@ -34,35 +34,38 @@ app.get("/", (req, res) =>
 
 //Let's see if this works without giving this error
 //MongoServerSelectionError: Server selection timed out after 30000 ms
-console.log("app.get/r-2019");
+console.log("app.get/R2019");
 app.get("/R2019/:detail", async (req, res) => {
     let detail = req.params.detail;
     console.log(detail);
     let data = await client.db("database1")
                 .collection("R-2019")
-                .find({ detail: detail });
+                .find({ detail: detail })
+                .toArray();
     console.log(data);
     return res.json(data);
 });
 
-console.log("app.get/r-2020");
-app.get("/R-2020/:detail", async (req, res) => {
+console.log("app.get/R2020");
+app.get("/R2020/:detail", async (req, res) => {
     let detail = req.params.detail;
     console.log(detail);
     let data = await client.db("database1")
                 .collection("R-2020")
-                .find({ detail: detail });
+                .find({ detail: detail })
+                .toArray();
     console.log(data);
     return res.json(data);
 });
 
-console.log("app.get/r-2021");
-app.get("/R-2021/:detail", async (req, res) => {
+console.log("app.get/R2021");
+app.get("/R2021/:detail", async (req, res) => {
     let detail = req.params.detail;
     console.log(detail);
     let data = await client.db("database1")
                 .collection("R-2021")
-                .find({ detail: detail });
+                .find({ detail: detail })
+                .toArray();
     console.log(data);
     return res.json(data);
 });
